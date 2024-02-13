07cf7b4324c88e9f2f261b3140b3cb07045bb42e
Trevor C. Justus <127045055+trevorcj@users.noreply.github.com>
2024-02-13 15:07:38 +0100

    Create dogToHumanAgeChecker.js

---
+++ dogToHumanAgeChecker.js
@@ -0,0 +1,8 @@
+// PROMPT FOR DOG AGE
+var dogAge = prompt('How old is your Dog?');
+
+// CALCULATE THE EQUIVALNT HUMAN AGE
+var humanAge = (dogAge - 2) * 4 + 21;
+
+// RETURN THE ANSWER
+alert('Your Dog will be ' + humanAge + ' years as a human!');
