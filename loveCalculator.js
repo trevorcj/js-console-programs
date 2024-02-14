d1b579d227df5e387e89e9add2386cab11ceb87b
Trevor C. Justus <127045055+trevorcj@users.noreply.github.com>
2024-02-14 13:01:30 +0100

    Create loveCalculator.js

---
+++ loveCalculator.js
@@ -0,0 +1,13 @@
+// LOVE CALCULATOR
+// ASK FOR THE NAMES OF THE TWO PEOPLE
+alert('Know if your relationship will work out!');
+
+prompt('Enter your First and Last name');
+prompt('Enter the First and Last name of your partner');
+
+// CALCULATE A RANDOM NUMBER (1 - 100%)
+var lovePercent = Math.random();
+lovePercent = Math.floor(lovePercent * 100);
+
+// GIVE THE INFORMATION BACK TO THE USER AS AN ALERT
+alert('Your Love Percent is ' + lovePercent + '%');
